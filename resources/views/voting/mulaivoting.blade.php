16f9ed189172724676b0758d5df0021615946ae7
hafid-nda <zaeinyh@gmail.com>
2023-08-06 22:42:19 +0700

    add mulaivoting

---
+++ resources/views/voting/mulaivoting.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Tambahkan link Bootstrap CSS (letakkan di dalam tag head) -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+    <title>EVOTING PILKETOS SMP NUSRA</title>
+        <link rel="stylesheet" href=" css/csslanding.css">
+</head>
+<body>
+   <!-- Nav -->
+   <nav>
+    
+      <img src="img/smpnusra.png" class="img-fluid" alt="Deskripsi Gambar Anda">
+    
+<hr>
+   </nav>
+   <!-- End Nav -->
+   <!-- Content -->
+   <section class="intro">
+        <div class="inner">
+            <div class="content mt-5">
+                <h1 style="color:#000000">Selamat Datang Di E-Voting Online Pemilihan Ketua Osis</h1>
+                <br>
+                <a href="votinglogin" class="mybtn">Mulai Voting</a>
+
+            </div>
+   </section>
+
+
+    <!-- Bootstrap  JS -->
+    <script src="js/app.js"></script>
+</body>
+</html>
